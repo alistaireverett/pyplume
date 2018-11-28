25abd8a5bf5b4693a1f4b14f3de82bb67ab79c6d
Alistair Everett <alistair.everett@gmail.com>
2018-11-28 10:47:54 +0100

    adding note to say this is probably broken!

--- examples/kronebreen_upwelling.py
+++ examples/kronebreen_upwelling.py
@@ -1,5 +1,9 @@
 #/usr/bin/env python
 
+############################################################################################
+#### !!!! THIS CODE IS PROBABLY BROKEN AT THE MOMENT, BUT WILL BE UPDATED SHORTLY !!!! #####
+############################################################################################
+
 import pyplume
 import math
 import numpy as np
